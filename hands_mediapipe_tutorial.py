70553b163e15f3934872d14dc5f4bb32bbbe0773
Ran Fang <ranf@andrew.cmu.edu>
2024-02-17 10:29:36 -0500

    feat: pose detection using holistic model

--- hands_mediapipe_tutorial.py
+++ hands_mediapipe_tutorial.py
@@ -55,15 +55,13 @@ mp_drawing = mp.solutions.drawing_utils # Drawing utilities
 
 def mediapipe_detection(image, model): 
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB 
-    image.flags.writable = False                 # Image is no longer writable 
+    image.flags.writeable = False                 # Image is no longer writable 
     results = model.process(image)                 # Make prediction 
-    image.flags.writable = True                 # Image is now writable 
+    image.flags.writeable = True                 # Image is now writable 
     image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR CONVERSION RGB 2 BGR 
     return image, results 
     
 def draw_landmarks(image, results): 
-    mp_drawing.draw_landmarks( 
-    image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS) # Draw face connections 
     mp_drawing.draw_landmarks( 
     image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections 
     mp_drawing.draw_landmarks( 
@@ -72,11 +70,6 @@ def draw_landmarks(image, results):
     image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections 
     
 def draw_styled_landmarks(image, results): 
-    # Draw face connections 
-    mp_drawing.draw_landmarks( 
-    image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS, 
-    mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1), 
-    mp_drawing.DrawingSpec(color=(80,256,121), thickness=1, circle_radius=1)) 
     # Draw pose connections 
     mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS, 
                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
@@ -93,7 +86,7 @@ def draw_styled_landmarks(image, results):
                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
                             ) 
 #Main function 
-cap = cv2.VideoCapture(0) 
+cap = cv2.VideoCapture(1) 
 # Set mediapipe model 
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic: 
     while cap.isOpened(): 
--- try_with_hands_and_pose.py
+++ try_with_hands_and_pose.py
@@ -11,17 +11,50 @@ model_path = 'hand_landmark.task'
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands_pose = mp.solutions.holistic
-     
+
+def mediapipe_detection(frame, model): 
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB 
+    frame.flags.writeable = False                 # frame is no longer writable 
+    results = model.process(frame)                 # Make prediction 
+    frame.flags.writeable = True                 # frame is now writable 
+    frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR) # COLOR CONVERSION RGB 2 BGR 
+    return frame, results 
+
+def draw_landmarks(image, results): 
+    mp_drawing.draw_landmarks( 
+    image, results.pose_landmarks, mp_hands_pose.POSE_CONNECTIONS) # Draw pose connections 
+    mp_drawing.draw_landmarks( 
+    image, results.left_hand_landmarks, mp_hands_pose.HAND_CONNECTIONS) # Draw left hand connections 
+    mp_drawing.draw_landmarks( 
+    image, results.right_hand_landmarks, mp_hands_pose.HAND_CONNECTIONS) # Draw right hand connections 
+
+def draw_styled_landmarks(image, results): 
+    # Draw pose connections 
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_hands_pose.POSE_CONNECTIONS, 
+                            mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
+                            mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2) 
+                            ) 
+    # Draw left hand connections 
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_hands_pose.HAND_CONNECTIONS, 
+                            mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
+                            mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2) 
+                            ) 
+    # Draw right hand connections 
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_hands_pose.HAND_CONNECTIONS, 
+                            mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
+                            mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2) 
+                            ) 
+
 # TEST: Load the video from specified path
 # cap = cv2.VideoCapture("video_test.mp4")
 # ACTION: Read live video from webcam
 cap = cv2.VideoCapture(1)
 start_time = time.time()
 frame_counter = 0
-with mp_hands_pose.Hands(
+with mp_hands_pose.Holistic(
     model_complexity=0,
     min_detection_confidence=0.5,
-    min_tracking_confidence=0.5) as hands:
+    min_tracking_confidence=0.5) as hands_pose:
     # TEST: mp4 feed-in case
     # while True:
     # ACTION: Live feed-in case
@@ -33,25 +66,15 @@ with mp_hands_pose.Hands(
             fps = frame_counter / (time.time() - start_time)
             cv2.putText(frame, f"FPS: {fps:.3f}", (30, 30), cv2.FONT_HERSHEY_PLAIN, 1.5, (0, 255, 255), 2, cv2.LINE_AA)
 
-            # To improve performance, optionally mark the image as not writeable to
-            # pass by reference.
-            frame.flags.writeable = False
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-            results = hands.process(frame)
+            # Make detections 
+            frame, results = mediapipe_detection(frame, hands_pose) 
+            # print(results) 
+        
+            # Draw landmarks 
+            draw_styled_landmarks(frame, results) 
 
-            # Draw the hand annotations on the frame.
-            frame.flags.writeable = True
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-            if results.multi_hand_landmarks:
-                for hand_landmarks in results.multi_hand_landmarks:
-                    mp_drawing.draw_landmarks(
-                        frame,
-                        hand_landmarks,
-                        mp_hands_pose.HAND_CONNECTIONS,
-                        mp_drawing_styles.get_default_hand_landmarks_style(),
-                        mp_drawing_styles.get_default_hand_connections_style())
             # Flip the frame horizontally for a selfie-view display.
-            cv2.imshow('MediaPipe Hands', cv2.flip(frame, 1))
+            cv2.imshow('MediaPipe Hands and Pose', cv2.flip(frame, 1))
             if cv2.waitKey(5) & 0xFF == 27:
                 break
         else:
