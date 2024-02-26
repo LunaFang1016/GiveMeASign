2a2879ec9993c988054fed61faaa637ecf805d03
Ran Fang <ranf@andrew.cmu.edu>
2024-02-26 12:04:45 -0500

    feat: re-create the xcode project

---
+++ GiveMeASign/GiveMeASign/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  GiveMeASign
+//
+//  Created by Ran Fang on 2/26/24.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+}
